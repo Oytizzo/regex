cf14866eb7b12fc3de4339324413da35d0c41c43
Oytizzo <oytizzo@gmail.com>
2021-03-30 23:21:24 +0600

    Normal search

---
+++ src/main.py
@@ -0,0 +1,9 @@
+import re
+
+s = "Afganistan, America, Bangladesh, Canada, Denmark, England, Greenland, Iceland, Netherlands, New Zealand, Sweden, Switzerland"
+
+countries = s.split(",")
+print(countries)
+
+li = [item for item in countries if item.endswith("land")]
+print(li)

947a0b7b4423b771b046733cafadd66c616b5d65
Oytizzo <oytizzo@gmail.com>
2021-03-30 23:27:26 +0600

    Fix bug

--- src/main.py
+++ src/main.py
@@ -8,5 +8,13 @@ s = "Afganistan, America, Bangladesh, Canada, Denmark, England, Greenland, Icela
 # li = [item for item in countries if item.endswith("land")]
 # print(li)
 
-li = re.findall(r'(\w+lands*)', s)
+# li = re.findall(r'(\w+lands*)', s)
+# print(li)
+
+li = re.findall(r'(\w*\s?\w+lands*)', s)
+print(li)
+
+# compiled re
+cpat = re.compile(r'(\w*\s?\w+lands*)')
+li = cpat.findall(s)
 print(li)

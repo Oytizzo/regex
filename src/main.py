ef35fb29391dd64160a1afc21ca7703208bef108
Oytizzo <oytizzo@gmail.com>
2021-03-30 23:24:20 +0600

    Regex used but search bug exists

--- src/main.py
+++ src/main.py
@@ -2,8 +2,11 @@ import re
 
 s = "Afganistan, America, Bangladesh, Canada, Denmark, England, Greenland, Iceland, Netherlands, New Zealand, Sweden, Switzerland"
 
-countries = s.split(",")
-print(countries)
+# countries = s.split(",")
+# print(countries)
 
-li = [item for item in countries if item.endswith("land")]
+# li = [item for item in countries if item.endswith("land")]
+# print(li)
+
+li = re.findall(r'(\w+lands*)', s)
 print(li)

7bbc4a2ef1ec214a4b1c7022021fde44508a5a78
Oytizzo <oytizzo@gmail.com>
2021-04-07 20:32:49 +0600

    Category scrapping added

---
+++ src/bookstore_crawler.py
@@ -0,0 +1,23 @@
+def get_page_content():
+
+def get_category_list():
+
+def get_book_list():
+
+def get_next_page():
+
+def get_product_details():
+
+def get_next_page():
+
+def scrape_book_info():
+
+def crawl_category():
+
+
+def crawl_website():
+    """crawl_website is the main function that coordinates the whole crawling task"""
+
+if __name__ == "__main__":
+    # Compile differenct regular expression patterns
+    crawl_website()
